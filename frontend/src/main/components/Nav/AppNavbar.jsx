d25f57a09d7a71e3cd26b63a7b969168b6d57961
Abhiram Agina <abhiram_agina@umail.ucsb.edu>
2025-11-04 23:29:57 -0800

    fixing AppNavbar

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -76,9 +76,9 @@ export default function AppNavbar({
                   </Nav.Link>
                   <Nav.Link as={Link} to="/recommendationrequests">
                     Recommendation Requests
+                    </Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
                     DiningCommonsMenuItem
-
                   </Nav.Link>
                 </>
               ) : (
