c296d4d9b3cb0dab46c04a0264b80c767c0870b8
Abhiram Agina <abhiram_agina@umail.ucsb.edu>
2025-11-09 13:43:11 -0800

    aa - fixing e2e test

--- src/test/java/edu/ucsb/cs156/example/web/RecommendationRequestsWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/RecommendationRequestsWebIT.java
@@ -3,8 +3,12 @@ package edu.ucsb.cs156.example.web;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 import edu.ucsb.cs156.example.WebTestCase;
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
+import edu.ucsb.cs156.example.repositories.RecommendationRequestRepository;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
@@ -16,24 +20,36 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("integration")
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RecommendationRequestsWebIT extends WebTestCase {
+  @Autowired RecommendationRequestRepository recommendationRequestRepository;
+
   @Test
   public void admin_user_can_create_edit_delete_recommendationrequests() throws Exception {
-    setupUser(true);
 
+    LocalDateTime ldt1 = LocalDateTime.parse("2025-10-28T00:00:00");
+    LocalDateTime ldt2 = LocalDateTime.parse("2025-10-29T00:00:00");
+
+    RecommendationRequest recommendationRequest1 =
+        RecommendationRequest.builder()
+            .requesterEmail("abhiram_agina@ucsb.edu")
+            .professorEmail("ziad.matni@ucsb.edu")
+            .explanation("This is required for M.S. Applications.")
+            .dateRequested(ldt1)
+            .dateNeeded(ldt2)
+            .done(false)
+            .build();
+
+    recommendationRequestRepository.save(recommendationRequest1);
+
+    setupUser(true);
     page.getByText("Recommendation Requests").click();
 
-    page.getByText("Create Recommendation Request").click();
-    assertThat(page.getByText("Create New Recommendation Request")).isVisible();
-    page.getByLabel("Requester Email").fill("abhiram_agina@ucsb.edu");
-    page.getByLabel("Professor Email").fill("ziad.matni@ucsb.edu");
-    page.getByLabel("Explanation").fill("This is required for P.H.D. Applications.");
-    page.getByLabel("Date Requested (iso format)").fill("2025-11-28T00:00:00");
-    page.getByLabel("Date Needed (iso format)").fill("2025-11-29T00:00:00");
-    page.getByLabel("Done").check();
-    page.getByText("Create").click();
-
-    assertThat(page.getByTestId("RecommendationRequestTable-cell-row-0-col-requesterEmail"))
+    assertThat(page.getByTestId("RecommendationRequestTable-cell-row-1-col-requesterEmail"))
         .hasText("abhiram_agina@ucsb.edu");
+
+    page.getByTestId("RecommendationRequestTable-cell-row-1-col-Delete-button").click();
+    assertThat(page.getByTestId("RecommendationRequestTable-cell-row-1-col-requesterEmail"))
+        .not()
+        .isVisible();
     /*
         page.getByTestId("RecommendationRequestTable-cell-row-0-col-requesterEmail").click();
         assertThat(page.getByText("Edit Restaurant")).isVisible();
